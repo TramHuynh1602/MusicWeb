a46e6ab1b4342c27592584d63bb7c8c795dba70b
Tram Huynh <18521519@gm.uit.edu.vn>
2022-08-17 12:18:55 +0700

    first commit

---
+++ JS/index.js
@@ -0,0 +1,19 @@
+const app = {
+    song: [
+            {
+                name: "Thanh Xuân",
+                path: "./assets/song/Thanh Xuân - Da LAB (Official MV).mp3",
+                image: "./assets/img/song/thanhxuan.jpg"
+            },
+            {
+                name: "Sài gòn đau lòng quá",
+                path: "./assets/song/'SÀI GÒN ĐAU LÒNG QUÁ' toàn kỷ niệm chúng ta... - HỨA KIM TUYỀN x HOÀNG DUYÊN (OFFICIAL MV).mp3",
+                image: "./assets/img/song/thanhxuan.jpg"
+            }
+        
+    
+    ],
+    
+
+
+}
