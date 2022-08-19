4f087a74175c00fd6299852d6fd38f06c98eb9fc
Tram Huynh <18521519@gm.uit.edu.vn>
2022-08-19 14:08:45 +0700

    play, pause, seek

--- JS/index.js
+++ JS/index.js
@@ -1,9 +1,16 @@
 const $ = document.querySelector.bind(document)
 const $$ = document.querySelectorAll.bind(document)
-const menuHeight = $('.menu').clientHeight
-const playList = $('.playlist')
+const menuHeight = $('.menu')
+const menuBody = $('.menu__body-img')
+const heading = $('.menu__header')
+const menuSongName = $('.menu__body-name')
+const audio = $('#audio')
+const btnPlay = $('.playbtn')
+const btnPause = $('.pausebtn')
+const progress = $('.progress')
 
 const app = {
+    currentIndex : 0,
     songs: [
             {
                 name: "Thanh Xuân",
@@ -58,6 +65,7 @@ const app = {
     
     ],
     render: function (){
+        console.log(234);
         const htmls = this.songs.map(song => {
             return `
             <div class="playlist__song-wrap">
@@ -78,30 +86,81 @@ const app = {
         })
         $('.playlist').innerHTML = htmls.join('')
     },
+    defineProperties: function (){
+        Object.defineProperty(this, 'currentSong', {
+            get: function() {
+                return this.songs[this.currentIndex]
+            }
+        })  
+    },
     handleEvent: function (){
-        const menuBody = $('.menu__body-img')
         const menuBodyWidth = menuBody.offsetWidth
         const menuBodyHeight = menuBody.offsetHeight
-        
+
+        // Xử lí phóng to thu nhỏ cd khi scroll
         document.onscroll = function () {
             const scrollTop = window.scrollY || document.documentElement.scrollTop
             const newMenuBodyWidth = menuBodyWidth - scrollTop;
             const newMenuBodyHeight = menuBodyHeight - scrollTop;
-            console.log(newMenuBodyHeight);
+            console.log(newMenuBodyHeight, newMenuBodyWidth)
             menuBody.style.width = newMenuBodyWidth > 0 ? newMenuBodyWidth + 'px' : 0;
             menuBody.style.height = newMenuBodyHeight > 0 ? newMenuBodyHeight + 'px' : 0;
-                
+            menuBody.style.opacity = newMenuBodyWidth / menuBodyWidth; 
             }
-        
+        // Xử lí khi click play
+        btnPlay.onclick = function() {
+            audio.play();
+        },
+        btnPause.onclick = function() {
+            audio.pause();
+        }
+        audio.onpause = function() {
+            btnPause.classList.remove('active')
+            btnPlay.classList.add('active');
+        }
+        audio.onplay = function() {
+            btnPause.classList.add('active')
+            btnPlay.classList.remove('active')
+        }   
+        // khi tien do bai hat thay doi
+        audio.ontimeupdate = function() {
+            if(audio.durations){
+            const progessPercent = Math.floor(audio.currentTime / audio.duration * 100)
+            progress.value = progessPercent;
+        }
+        }
+        progress.onchange = function(e) {
+            var seek = (audio.duration / 100 * e.target.value)
+            audio.currentTime= seek
+
+        }
+    },
+    loadCurrentSong: function (){
+        // console.log(this.currentSong)
+        menuSongName.textContent = this.currentSong.name
+        menuBody.style.backgroundImage = `url('${this.currentSong.image}')`
+        audio.src = this.currentSong.path
+
     },
     // gget playlist
     fixPlayList: function(){
-        playList.style.marginTop = menuHeight + 10 +'px'
+        const playList = $('.playlist')
+        playList.style.marginTop = menuHeight.clientHeight +'px'
+        console.log(menuHeight.clientHeight)
     },
     start : function () {
-        this.render();
+        // Định nghĩa các thuộc tính cho Object
+        this.defineProperties();
+        // Lắng nghe và xử lí các sự kiện (DOM Events)
         this.handleEvent();
+        // Tải thông tin bài hát đầu tiên vào UI khi chạy ứng dụng
+        this.loadCurrentSong();
+        // Render lại playlist
+        this.render();
         this.fixPlayList();
+        // console.log(menuHeight);
+        
+
     }
 
 }
